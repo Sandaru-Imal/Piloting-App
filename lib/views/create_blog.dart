c40ce1ca5d11f27b453059edde197adaa99b1f13
Sandaru Imal <43520816+Sandaru-Imal@users.noreply.github.com>
2020-07-25 22:16:03 +0530

    cloud firestore upgrade

--- lib/views/create_blog.dart
+++ lib/views/create_blog.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:piloting_app/services/crud.dart';
 
 class CreateBlog extends StatefulWidget {
   @override
@@ -7,6 +10,19 @@ class CreateBlog extends StatefulWidget {
 
 class _CreateBlogState extends State<CreateBlog> {
   String authorName, title, desc;
+  File selectedImage;
+  CrudMethods crudMethods = new CrudMethods();
+
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      selectedImage = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,18 +53,36 @@ class _CreateBlogState extends State<CreateBlog> {
             SizedBox(
               height: 10,
             ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(6),
-              ),
-              width: MediaQuery.of(context).size.width,
-              child: Icon(
-                Icons.add_a_photo,
-                color: Colors.black,
-              ),
+            GestureDetector(
+              onTap: () {
+                getImage();
+              },
+              child: selectedImage != null
+                  ? Container(
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      height: 150,
+                      width: MediaQuery.of(context).size.width,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: Image.file(
+                          selectedImage,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  : Container(
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      child: Icon(
+                        Icons.add_a_photo,
+                        color: Colors.black,
+                      ),
+                    ),
             ),
             SizedBox(
               height: 8,
